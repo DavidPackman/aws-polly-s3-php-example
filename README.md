233a6ad3bda46dde6430cb398fdaf0a987372bc8
David <david@easyblog.org>
2017-05-24 14:35:53 +0800

    Initial commit
    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# aws-polly-s3-php-example
+# AWS Polly S3 PHP Example
 Example PHP script thats uses AWS Polly to convert a string into an mp3 file and saves it to S3.
+
+## Requirements
+Requires [AWS PHP SDK v3](https://docs.aws.amazon.com/aws-sdk-php/v3/guide/).
+Depending on your chosen method of installing the SDK you may need to alter the following line: `require_once 'aws/aws-autoloader.php';`
+
+## Basic Configuration
+You will need to modify the example.php file to include your AWS security credentials.  The IAM user must have access to both S3 and Polly.
+
+The following lines will need modification.
+
+```
+$awsAccessKeyId = 'XXXXXX';
+$awsSecretKey   = 'XXXXXX';
+$s3bucket = 'bucketname';
+$s3region = 'yourbucketregion'
+```
+## Running the example
+This example has been setup to run as a webpage.  You will need to upload the code, including the AWS PHP SDK to a web server and then access `example.php`.  When you load the page it will automatically pass the text to Polly and save the returned mp3 file to S3.  The webpage will display a audio control to listen to the file.   
---
+++ example.php
@@ -0,0 +1,52 @@
+<?php
+#Setup and Credential settings
+require_once 'aws/aws-autoloader.php';
+$awsAccessKeyId = 'XXXXXX';
+$awsSecretKey   = 'XXXXXX';
+$credentials    = new \Aws\Credentials\Credentials($awsAccessKeyId, $awsSecretKey);
+
+#Polly Text to Speach Code
+$client_polly         = new \Aws\Polly\PollyClient([
+  'version'     => '2016-06-10',
+  'credentials' => $credentials,
+  'region'      => 'us-east-1',
+]);
+
+$result_polly         = $client_polly->synthesizeSpeech([
+  'OutputFormat' => 'mp3',
+  'Text'         => "Hello, this is an example PHP script which saves an mp3 file created by AWS Polly to your choosen S3 bucket.",
+  'TextType'     => 'text',
+  'VoiceId'      => 'Amy',
+]);
+
+#Returned audio data
+$resultData_polly     = $result_polly->get('AudioStream')->getContents();
+
+#Save MP3 to S3
+$s3bucket = 'bucketname';
+$s3region = 'yourbucketregion'
+$filename = time().'-polly.mp3';
+
+$client_s3 = new Aws\S3\S3Client([
+  'version'     => 'latest',
+  'credentials' => $credentials,
+  'region'      => $s3region
+]);
+
+$result_s3 = $client_s3->putObject([
+  'Key' => $filename,
+  'ACL' => 'public-read',
+  'Body' => $resultData_polly,
+  'Bucket' => $s3bucket,
+  'ContentType' => 'audio/mpeg',
+  'SampleRate' => '8000'
+]);
+
+?>
+<html>
+<body>
+  <audio controls>
+    <source src="<?php echo $result_s3['ObjectURL'] ?>" type="audio/mpeg">
+  </audio>
+</body>
+</html>
